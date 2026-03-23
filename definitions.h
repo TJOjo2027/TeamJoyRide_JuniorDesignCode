37f52776052a29b4d700a498105de290d22036aa
TJOjo2027 <tj2k27@gmail.com>
2026-03-23 13:32:13 -0500

    Small Changes to Definitions

--- definitions.h
+++ definitions.h
@@ -5,12 +5,24 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-// MOTOR PINS
-
 // LINEAR ACUATOR PINS
 
 // CLAW PINS
 
+// SENSOR PINS
+const uint8_t IR_SENSOR_PIN = 40;
+
+// LIMIT SWITCH PINS
+const uint8_t LIMIT_SWITCH_PIN_1 = 30;
+const uint8_t LIMIT_SWITCH_PIN_2 = 31;
+
+// STEPPER DRIVER PINS
+const uint8_t PUL_DRIVER_PIN_1 = 22;
+const uint8_t DIR_DRIVER_PIN_1 = 23;
+const uint8_t PUL_DRIVER_PIN_2 = 24;
+const uint8_t DIR_DRIVER_PIN_2 = 25;
+const uint8_t PUL_DRIVER_PIN_3 = 26;
+const uint8_t DIR_DRIVER_PIN_3 = 27;
 
 
 #endif //DEFINITIONS_H
